e53bff8638c813be5f45302f26b0f1a66628ac1a
3iqpotato <103383079+3iqpotato@users.noreply.github.com>
2024-11-16 21:28:55 +0200

    add: added admin views for questions and answers

--- PartyHub_Project/PartyHub_Project/Questions/admin.py
+++ PartyHub_Project/PartyHub_Project/Questions/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'author', 'party', 'created_at', 'get_answer')
+    list_filter = ('created_at', 'party')
+    search_fields = ('text', 'author__username',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+    def get_answer(self, obj):
+        return f"{obj.answer.text} | answer from  :{obj.answer.author.username}" \
+            if hasattr(obj, 'answer') else 'No answer yet'
+    get_answer.short_description = 'Answer'
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('text', 'author', 'question', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('text', 'author__username', 'question__text')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
